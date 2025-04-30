1788c3635e647750c2448bf20ac16f26be0107c0
1308harshit <khatsuriyaharshit@gmail.com>
2025-04-30 17:04:29 +0530

    locally terraform working

---
+++ terraform_10/variable.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ecr_image" {
+  description = "The full ECR image URI including tag to deploy"
+  type        = string
+
+}
+
+variable "app_port" {
+  default = 1337
+}
