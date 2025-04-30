c778fbd1a3e0b23fbe9721c0192b5658d06d8ad4
1308harshit <khatsuriyaharshit@gmail.com>
2025-04-30 12:20:54 +0530

    updating code base and deploy.yml file

--- terraform10/iam.tf
+++ terraform10/iam.tf
@@ -23,42 +23,44 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-resource "aws_iam_role" "codedeploy_role" {
-  name = "codedeploy-ecs-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "codedeploy.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codedeploy_role_attach" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
-}
-
-# resource "aws_iam_role" "codedeploy_service_role" {
-#   name = "CodeDeployECSServiceRole"
+# resource "aws_iam_role" "codedeploy_role" {
+#   name = "codedeploy-ecs-role"
 
 #   assume_role_policy = jsonencode({
 #     Version = "2012-10-17",
 #     Statement = [{
+#       Action = "sts:AssumeRole",
 #       Effect = "Allow",
 #       Principal = {
 #         Service = "codedeploy.amazonaws.com"
-#       },
-#       Action = "sts:AssumeRole"
+#       }
 #     }]
 #   })
 # }
 
-# resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
-#   role       = aws_iam_role.codedeploy_service_role.name
+# resource "aws_iam_role_policy_attachment" "codedeploy_role_attach" {
+#   role       = aws_iam_role.codedeploy_role.name
 #   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 # }
+
+# -------------------------------------------------------
+
+resource "aws_iam_role" "codedeploy_service_role" {
+  name = "CodeDeployECSServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+  role       = aws_iam_role.codedeploy_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
--- terraform10/main.tf
+++ terraform10/main.tf
@@ -10,4 +10,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
